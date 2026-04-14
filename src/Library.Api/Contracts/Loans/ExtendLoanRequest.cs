56f29b67b8cfd8a429526911ff84a48546fae648
Jeremiasz Kubryn <jeremiaszkubryn@gmail.com>
2026-04-14 18:17:35 +0200

    loan extension

---
+++ src/Library.Api/Contracts/Loans/ExtendLoanRequest.cs
@@ -0,0 +1,3 @@
+namespace Library.Api.Contracts.Loans;
+
+public sealed record ExtendLoanRequest(DateOnly DueDate);
--- src/Library.Domain/Entities/Loan.cs
+++ src/Library.Domain/Entities/Loan.cs
@@ -67,6 +67,23 @@ public sealed class Loan
         ReturnedAtUtc = normalizedReturnedAtUtc;
     }
 
+    public void ExtendTo(DateTime dueDateUtc)
+    {
+        if (IsReturned)
+        {
+            throw new DomainRuleException("Returned loan cannot be extended.");
+        }
+
+        var normalizedDueDateUtc = EnsureUtc(dueDateUtc);
+
+        if (normalizedDueDateUtc <= DueDateUtc)
+        {
+            throw new DomainRuleException("New due date must be later than current due date.");
+        }
+
+        DueDateUtc = normalizedDueDateUtc;
+    }
+
     private static DateTime EnsureUtc(DateTime value)
     {
         return value.Kind switch
